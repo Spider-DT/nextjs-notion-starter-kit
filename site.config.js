5ee086de77d0f98ce1bae2e6225510c0a5e5d5aa
Little_wang <60735795+Spider-DT@users.noreply.github.com>
2021-09-07 09:52:37 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 'fde5ac74eea345278f004482710e1af3',
+  rootNotionSpaceId: null,
 
   // basic site info (required)
   name: 'Learn Share Write',
